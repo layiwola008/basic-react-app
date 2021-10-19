28c3af8557e739f1773531e60dccd5def0829eb8
Olayiwola Adepoju <78272392+layiwola008@users.noreply.github.com>
2021-10-19 14:44:00 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// //basic starting code 
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import './index.css'
+// import RouletteGun from './state-drills/RouletteGun'
+
+// ReactDOM.render(
+  
+//         <RouletteGun />
+//         ,
+//   document.getElementById('root')
+// )
+
+//2.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+ReactDOM.render(
+  <h1> {2 + 3}</h1> ,
+  document.getElementById('app')
+); 
